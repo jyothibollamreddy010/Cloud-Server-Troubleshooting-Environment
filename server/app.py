da0fc82ae5f823ee771c194667a53d138537dd92
jyothibollamreddy010 <jyothibollamreddy010@gmail.com>
2026-04-08 22:41:43 +0530

    Clean up imports in app.py
    Removed unnecessary import and comment.

--- server/app.py
+++ server/app.py
@@ -1,10 +1,7 @@
-import uvicorn
-from app import app
-
-# rebuild   👈 you add this line
-
 def main():
+    import uvicorn
     uvicorn.run("app:app", host="0.0.0.0", port=8000)
+
 
 if _name_ == "_main_":
     main()
