6e58afb6bcfb04c14ae23fb8c09a3cbc8b111e69
jyothibollamreddy010 <jyothibollamreddy010@gmail.com>
2026-04-08 22:38:20 +0530

    Modify uvicorn.run to use string reference for app
    Updated the main function to run the app using a string reference.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
-from app import app
 import uvicorn
+from app import app
+
+# rebuild   👈 you add this line
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
 
 if _name_ == "_main_":
     main()
